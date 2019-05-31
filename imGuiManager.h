c44c820920a80e8828c4ce112f2bc739383f1b94
YB <whyis2cool@gmail.com>
2019-05-31 08:14:49 -0500

    last commit before i make a sampler class

--- imGuiManager.h
+++ imGuiManager.h
@@ -15,9 +15,11 @@ public:
 	imGuiManager();
 	~imGuiManager();
 public:
+	/*! start the imgui class*/
 	bool Init(CDevice &Device, CDeviaceContext &DeviceContext, HWND& Handle);
 	//! has nothing just the simplest window you can make 
 	void MakeBasicWindow(const char* WindowName);
+	//! here is a window that contains very specific information 
 	void MakeWindowFpsAndVertexCount(const char* WindowName, float DeltaTime, int VertexCount);
 
 	
